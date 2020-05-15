b4a1cee0464b1f0d7407bb08fc1d0a2bf6ca2581
nixholas <nixholas@icloud.com>
2020-05-15 21:59:42 +0800

    src: views: auth: Logout: Initial implementation
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ src/views/auth/Logout.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="hero is-medium">
+    <div class="hero-body">
+      <h2>Logging you out, hang on..</h2>
+    </div>
+  </section>
+</template>
+
+
+<script>
+  import Firebase from 'firebase';
+  import { mapState } from 'vuex';
+
+  export default {
+    name: 'Logout',
+    computed: {
+      ...mapState({
+        user: 'user',
+      }),
+    },
+    beforeMount() {
+      if (!user) this.$router.replace( {name: 'home'} );
+    },
+    mounted() {
+      Firebase.auth().signOut()
+        .then(() => {
+          this.$router.replace({
+            name: 'home'
+          });
+        });
+    },
+  };
+</script>
