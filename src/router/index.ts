6c2ce0cd6023e63f69c9ab0456f81dd6307fc6b9
nixholas <nixholas@icloud.com>
2020-05-15 22:02:18 +0800

    src: router: index: guard logout
    Signed-off-by: nixholas <nixholas@icloud.com>

--- src/router/index.ts
+++ src/router/index.ts
@@ -41,6 +41,7 @@ const routes: Array<RouteConfig> = [
   {
     path: '/logout',
     name: 'Logout',
+    beforeEnter: guardRoute,
     // route level code-splitting
     // this generates a separate chunk (logout.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
