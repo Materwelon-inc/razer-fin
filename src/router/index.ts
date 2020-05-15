f446ca06e362eea3c35ebc8a106c8c27f623f671
nixholas <nixholas@icloud.com>
2020-05-15 18:21:40 +0800

    src: router: index: Register routing
    Signed-off-by: nixholas <nixholas@icloud.com>

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '../views/Home.vue';
+import Register from '../views/auth/Register.vue';
 
 Vue.use(VueRouter);
 
@@ -19,12 +20,17 @@ const routes: Array<RouteConfig> = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
   {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+  },
+  {
     path: '/login',
     name: 'Login',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (login.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue'),
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue'),
   },
 ];
 
