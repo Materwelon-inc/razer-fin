f774515fbcaa557ac47c2a830d0f70f65600b2f9
nixholas <nixholas@icloud.com>
2020-05-15 20:24:37 +0800

    src: router: index: router-store-based route guard
    Signed-off-by: nixholas <nixholas@icloud.com>

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,15 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
+import store from '../store';
 import Home from '../views/Home.vue';
 import Register from '../views/auth/Register.vue';
 
 Vue.use(VueRouter);
+
+function guardRoute(to: any, from: any, next: any) {
+  if (to.name !== 'Login' && !store.state.user.loggedIn) next({ name: 'Login' });
+  else next();
+}
 
 const routes: Array<RouteConfig> = [
   {
@@ -31,6 +37,15 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (login.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "login" */ '../views/auth/Login.vue'),
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    beforeEnter: guardRoute,
+    // route level code-splitting
+    // this generates a separate chunk (dashboard.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "dashboard" */ '../views/dashboard/Index.vue'),
   },
 ];
 
