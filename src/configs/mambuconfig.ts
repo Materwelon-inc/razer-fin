e2efccf7d61672931274a7dda3a7d01f27e8e227
Nicholas <nixholas@icloud.com>
2020-05-16 17:16:48 +0800

    src: configs: mambuconfig: Initial implementation
    Signed-off-by: Nicholas <nixholas@icloud.com>

---
+++ src/configs/mambuconfig.ts
@@ -0,0 +1,9 @@
+export class MambuConfig {
+  static RequiredProperties = [ 'first_name', 'last_name', 'preferred_language', 'notes', 'assigned_branch_key', 'country_of_birth' ];
+  static MambuProducts = {
+    CurrentAccount: '8a8e878471bf59cf0171bf6979700440',
+    EndowmentPlan: '8a8e867271bd280c0171bf768b9c1a81',
+    SmallTermLoan: '8a8e867271bd280c0171bf768cc31a89',
+    LargeTermLoan: '8a8e878e71c7a4d70171c85a697b01f7',
+  };
+};
