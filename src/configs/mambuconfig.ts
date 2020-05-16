c7f6ea964081da98f5768ecc819ef75abf887976
Nicholas <nixholas@icloud.com>
2020-05-16 21:56:15 +0800

    src: configs: property key array introductions
    Signed-off-by: Nicholas <nixholas@icloud.com>

--- src/configs/mambuconfig.ts
+++ src/configs/mambuconfig.ts
@@ -6,6 +6,8 @@ export class MambuConfig {
   static branchId = 'team54';
   static branchKey = '8a8e878e71c7a4d70171ca6253701208';
   static RequiredProperties = [ 'first_name', 'last_name', 'preferred_language', 'notes', 'assigned_branch_key', 'country_of_birth' ];
+  static documentProperties = [];
+  static customInformationProperties = [];
   static MambuProducts = {
     CurrentAccount: '8a8e878471bf59cf0171bf6979700440',
     EndowmentPlan: '8a8e867271bd280c0171bf768b9c1a81',
